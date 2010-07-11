2b3c13726dddff808fd906ff2de05e086f793055
Dominic Rausch <Dominic.rausch@googlemail.com>
2010-07-11 15:32:27 +0200

    Added some UI classes

---
+++ Hermes/src/hermes/view/BuddyList.java
@@ -0,0 +1,5 @@
+package hermes.view;
+
+public class BuddyList {
+
+}
---
+++ Hermes/src/hermes/view/View.java
@@ -0,0 +1,12 @@
+package hermes.view;
+
+import javax.swing.*;
+
+public class View extends JFrame{
+    
+    public View() {
+	
+	this.setLayout(new BorderLayout());
+	
+    }
+}
